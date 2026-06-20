51a338c9e2db29296d9671f49cb76571daad2d5c
Leandro Andrade <113956647+leanttro@users.noreply.github.com>
2026-06-20 15:50:25 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
+import './no-horizontal-scroll.css';
 import { createRoot } from 'react-dom/client';
 import './styles/tokens.css';
 import './i18n/i18n.js';
 import App from './App.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
